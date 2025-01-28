2b997b1cb20326debb084fcf79252378dd725643
jhwan0707 <vaingloryexdy@gmail.com>
2025-01-28 21:53:04 +0800

    Add, List

--- src/main/java/Johan.java
+++ src/main/java/Johan.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Johan {
     public static void main(String[] args) {
+        String input;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Johan");
         System.out.println("What can I do for you?");
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println(input);
+        ArrayList<String> words = new ArrayList<>();
+        while (true) {
             input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                int count = 0;
+                for (String word : words) {
+                    System.out.println(++count + ". " + word);
+                }
+            } else {
+                System.out.println("added: " + input);
+                words.add(input);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }

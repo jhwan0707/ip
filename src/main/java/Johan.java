0635185cd6ffd6fad744d212dd0702f0b8bae786
jhwan0707 <vaingloryexdy@gmail.com>
2025-01-29 21:31:22 +0800

    Mark as Done

--- src/main/java/Johan.java
+++ src/main/java/Johan.java
@@ -7,19 +7,36 @@ public class Johan {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Johan");
         System.out.println("What can I do for you?");
-        ArrayList<String> words = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         while (true) {
-            input = scanner.nextLine();
+            input = scanner.nextLine().toLowerCase().trim();
+            String[] words = input.split("");
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
-                int count = 0;
-                for (String word : words) {
-                    System.out.println(++count + ". " + word);
+                System.out.println("Here are the tasks in your list:");
+                for (Task t : tasks) {
+                    System.out.println(t.getID() + ". [" + t.getStatusIcon() + "] " + t.toString());
+                }
+            } else if (input.startsWith("mark ")) {
+                String taskID = input.substring(5);
+                int id = Integer.parseInt(taskID);
+                if (id > 0 && id <= tasks.size()) {
+                    tasks.get(id - 1).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + tasks.get(id - 1).getStatusIcon() + "] " + tasks.get(id - 1).toString());
+                }
+            } else if (input.startsWith("unmark ")) {
+                String taskID = input.substring(7);
+                int id = Integer.parseInt(taskID);
+                if (id > 0 && id <= tasks.size()) {
+                    tasks.get(id - 1).markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("[" + tasks.get(id - 1).getStatusIcon() + "] " + tasks.get(id - 1).toString());
                 }
             } else {
                 System.out.println("added: " + input);
-                words.add(input);
+                tasks.add(new Task(input));
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    private static int nextTaskID = 1;
+    private final int ID;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        this.ID = nextTaskID++;
+    }
+
+    public String getID() {
+        return Integer.toString(this.ID);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
